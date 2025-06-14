4b792c7f994267f9688e702a51a0971edbace052
sumaiyaahmedpew <nahinfatema1@gmail.com>
2025-06-14 18:26:55 +0600

    Create 6-tuples.py

---
+++ 6-tuples.py
@@ -0,0 +1,14 @@
+# Creating a tuple of colors
+colors = ("red", "green", "blue")
+
+# Accessing elements
+print("First color:", colors[0])
+print("Last color:", colors[-1])
+
+# Trying to modify a tuple (will cause error if uncommented)
+# colors[0] = "yellow"
+
+# Loop through tuple
+print("All colors:")
+for color in colors:
+    print(color)
