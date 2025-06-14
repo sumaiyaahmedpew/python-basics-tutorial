c626104fa7bf854383704f3fa993a5870118c066
sumaiyaahmedpew <nahinfatema1@gmail.com>
2025-06-14 18:06:30 +0600

    Create 4-lists.py

---
+++ 4-lists.py
@@ -0,0 +1,20 @@
+# Creating a list of fruits
+fruits = ["apple", "banana", "cherry", "mango"]
+
+# Accessing elements
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+
+# Adding a new fruit
+fruits.append("orange")
+print("After adding:", fruits)
+
+# Removing a fruit
+fruits.remove("banana")
+print("After removing:", fruits)
+
+# Looping through list
+print("All fruits:")
+for fruit in fruits:
+    print(fruit)
+
