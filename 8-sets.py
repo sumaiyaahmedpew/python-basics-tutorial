8d8db012aa1e23cc1dd9c5473a426226676ed761
sumaiyaahmedpew <nahinfatema1@gmail.com>
2025-06-14 18:30:31 +0600

    Create 8-sets.py

---
+++ 8-sets.py
@@ -0,0 +1,19 @@
+# Creating a set of numbers
+numbers = {1, 2, 3, 4, 5}
+
+# Adding a new number
+numbers.add(6)
+
+# Removing a number
+numbers.remove(3)
+
+# Duplicate values are automatically removed
+numbers.add(2)
+
+# Print all elements
+print("Set of numbers:", numbers)
+
+# Loop through the set
+print("All numbers:")
+for number in numbers:
+    print(number)
