cbb55840cf15e87e0515e8f72e7d30724918a04b
Sumiaya Ahmed <nahinfatema1@gmail.com>
2025-06-19 01:09:46 +0600

    Create 17-bitwise.py

---
+++ 17-bitwise.py
@@ -0,0 +1,9 @@
+# Bitwise operators in Python
+a = 10  # 1010
+b = 4   # 0100
+print("AND:", a & b)      # 0000
+print("OR:", a | b)       # 1110
+print("XOR:", a ^ b)      # 1110
+print("NOT a:", ~a)
+print("Left Shift a by 2:", a << 2)
+print("Right Shift b by 1:", b >> 1)
