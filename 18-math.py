2f5247eb63dc47e6d8f25b69884417f1cca15a66
Sumiaya Ahmed <nahinfatema1@gmail.com>
2025-06-19 01:10:17 +0600

    Create 18-math.py

---
+++ 18-math.py
@@ -0,0 +1,6 @@
+# Using math module
+import math
+print("Square root of 16:", math.sqrt(16))
+print("Power 2^3:", math.pow(2, 3))
+print("Sine of 90 degrees:", math.sin(math.radians(90)))
+print("Log base 10 of 100:", math.log10(100))
