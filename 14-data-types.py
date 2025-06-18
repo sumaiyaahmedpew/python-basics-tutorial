538694e8b5c385ecc4f1fbf934a948b1a9cd74d3
Sumiaya Ahmed <nahinfatema1@gmail.com>
2025-06-19 01:08:02 +0600

    Create 14-data-types.py

---
+++ 14-data-types.py
@@ -0,0 +1,10 @@
+# Demonstrating various data types
+name = "Python"      # string
+age = 25             # integer
+height = 5.8         # float
+is_programmer = True # boolean
+skills = ["Python", "SQL"] # list
+info = (1, 2, 3)     # tuple
+marks = {90, 85, 80} # set
+details = {"name": "Sumaiya", "age": 22} # dictionary
+print(type(name), type(age), type(height), type(is_programmer))
