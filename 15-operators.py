7ecf61808c490d6519387eab58af255f67e97c48
Sumiaya Ahmed <nahinfatema1@gmail.com>
2025-06-19 01:08:44 +0600

    Create 15-operators.py

---
+++ 15-operators.py
@@ -0,0 +1,11 @@
+# Using arithmetic and logical operators
+a = 15
+b = 4
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+print("Is a > b and b < 10?", a > b and b < 10)
