9ef5193c2d9d31481a670ef30ed18f562d2be1b2
Sumiaya Ahmed <nahinfatema1@gmail.com>
2025-06-22 02:25:23 +0600

    Create 30-why-numpy.py

---
+++ 30-why-numpy.py
@@ -0,0 +1,6 @@
+# Why NumPy is useful (demo with list vs numpy array)
+import numpy as np
+list1 = [1, 2, 3, 4]
+np_arr = np.array(list1)
+print("List multiplied:", list1 * 2)
+print("NumPy array multiplied:", np_arr * 2)
