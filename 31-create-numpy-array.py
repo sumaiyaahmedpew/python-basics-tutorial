b4203b25f3b9a849a35de3ed729c33d6e0aecdd2
Sumiaya Ahmed <nahinfatema1@gmail.com>
2025-06-22 02:26:26 +0600

    Create 31-create-numpy-array.py

---
+++ 31-create-numpy-array.py
@@ -0,0 +1,12 @@
+# Creating numpy arrays
+import numpy as np
+arr1 = np.array([1, 2, 3])
+arr2 = np.zeros(3)
+arr3 = np.ones(3)
+arr4 = np.arange(0, 10, 2)
+arr5 = np.linspace(1, 5, 4)
+print("Array:", arr1)
+print("Zeros:", arr2)
+print("Ones:", arr3)
+print("Arange:", arr4)
+print("Linspace:", arr5)
